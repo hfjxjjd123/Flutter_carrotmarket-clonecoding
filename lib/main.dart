f4be883cfe95c454caebacd67ca49a5b2fec8ede
HakRimLee <hakrim0504@gmail.com>
2022-06-23 21:06:28 +0900

    --init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ReloadInfinitly(),
+    );
+  }
+}
+
+class ReloadInfinitly extends StatelessWidget {
+  const ReloadInfinitly({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
