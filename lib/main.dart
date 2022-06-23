26eda45a7726ee04ee2fe65d0cdb0e68001a04a9
HakRimLee <hakrim0504@gmail.com>
2022-06-23 21:46:35 +0900

    --packageincluding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice1/splash_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -10,18 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ReloadInfinitly(),
+      home: SplashScreen(),
     );
   }
 }
 
-class ReloadInfinitly extends StatelessWidget {
-  const ReloadInfinitly({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: CircularProgressIndicator()),
-    );
-  }
-}
